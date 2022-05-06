fe60aaaceabd90144eeb3b9193263cce07e014fc
alexdrZ21 <100728040+alexdrZ21@users.noreply.github.com>
2022-05-06 18:27:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,47 @@
 #include <fstream>
 #include <iomanip>
 #include <windows.h>
-
+#include <streambuf>
 
 using namespace std;
 
-string readFile(const string& fileName)
+void del_conv(ifstream& inp, ofstream& outp)
 {
-    ifstream f(fileName);
-    stringstream sst;
-    sst << f.rdbuf();
-    return sst.str();
+    char x;
+    while (inp.get(x))
+    {
+        if (x == ',')
+            x = ' ';
+        outp << x;
+    }
 }
+
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    string delim(".:;,()-?!'");
-    string str;
+    setlocale(LC_ALL, "rus");
+    string delim(".:;&#,()*-?!'");
+    ifstream t("D:/pushkin-metel.txt");
+    string str((std::istreambuf_iterator<char>(t)),
+        istreambuf_iterator<char>());
     map<string, int> freq; // словарь (ключ/значение)
-    cout << "string" << endl;
-    getline(cin, str);
     int str_count = 0; // число слов
     int index;
     while ((index = str.find_first_of(delim)) != string::npos) // замена всех стоп знаков на пробелы
         str[index] = ' ';
     // строки делим на слова, помещаем слова в слолварь(ключ/значение)
+    
     stringstream ss(str);
+    ofstream out("D:/out.txt");
+    ifstream del("D:/del.txt");
+    ofstream _del("D:/del2.txt");
+    del_conv(del, _del);
+    _del.close();
+    ifstream delet("D:/del2.txt");
+    while (delet >> str) // удаление стоп слов
+    {
+        freq.erase(str);
+    }
     while (ss >> str)
     {
         ++str_count;
@@ -40,8 +55,9 @@ int main()
     for (map<string, int>::iterator iter = freq.begin(); iter != freq.end(); ++iter) // подсчёт частоты
     {
         double p = static_cast<double>(iter->second) / static_cast<double>(str_count) * 100;
-        cout << iter->first << ": " << p << "%" << endl;
+        out << iter->first << ": " << p << "%" << endl;
     }
     system("pause"); //если не указать, то консоль вылетит
+
     return 0;
 }
